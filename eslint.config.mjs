109cf1b5727d3344b17536a378716a6a4c6e3ee3
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-08-30 07:43:42 +0700

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,9 @@ import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  { 
+  {
     languageOptions: { globals: globals.browser },
   },
   pluginJs.configs.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ];
