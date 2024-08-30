9f08609c249221a0c9558e54f52e7e193500a492
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-08-30 07:42:33 +0700

    Configure prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # js-lint-prettier-template
+
 ## This is a template!
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { 
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
+  eslintConfigPrettier
 ];
